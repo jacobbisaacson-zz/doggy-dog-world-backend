17ab657dcce651af8eca63a7dc11847b6aa15ddf
Jacob Isaacson <jacobisaacson@Jacobs-MacBook-Pro.local>
2020-05-07 12:23:56 -0500

    mr postman -- dogs resource working, hitting create dog route

--- resources/dogs.py
+++ resources/dogs.py
@@ -3,6 +3,12 @@ from flask import Blueprint
 
 dogs = Blueprint('dogs', 'dogs')
 
-@dogs.route('/')
+# INDEX
+@dogs.route('/', methods=['GET'])
 def dogs_index():
 	return "dogs resource working"
+
+# CREATE
+@dogs.route('/', methods=['POST'])
+def create_dog():
+	return "hitting create_dog route"
